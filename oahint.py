7f5072969098c49af2f36ea226060cbdcb25a947
JuziBanfan <501600056@qq.com>
2025-05-30 13:26:31 +0800

    Add files via upload

---
+++ oahint.py
@@ -0,0 +1,24 @@
+import requests
+import json
+ 
+def bot(robot_webhook,text):
+    # 构建消息内容
+    message_data = {
+        "msgtype": "text",
+        "text": {
+            "content": text,
+            "mentioned_list": ["@all"]
+        }
+    }
+    # 发送请求
+    response = requests.post(robot_webhook, headers={"Content-Type": "application/json"}, data=json.dumps(message_data))
+ 
+    # 检查响应状态码
+    if response.status_code == 200:
+        print("消息发送成功")
+    else:
+        print(f"消息发送失败，状态码：{response.status_code}")
+if __name__ == '__main__':
+    url = 'https://qyapi.weixin.qq.com/cgi-bin/webhook/send?key=ce352bce-d4ac-40c8-bd84-25c45f96e44b'
+    text = "git actions 测试"
+    bot(url, text)
